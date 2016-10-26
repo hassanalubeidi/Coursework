85d517f47ce6527bd37e15589f1d8d2866fc87b2
Hassan Al-ubeidi <psyha3@linode123.cs.nott.ac.uk>
2016-10-26 14:35:09 +0100

    fixed smallest_money

--- Year1/2/smallest_money.c
+++ Year1/2/smallest_money.c
@@ -1,73 +1,57 @@
 #include <stdio.h>
+#include <math.h>
+void smallest_money(int pounds); int number_of_positives(int binary_integer); void reset_splits(); char plural(int);
+const int denominations[4] = {20,10,5,1};
+int splits[4] = {0,0,0,0};
+int main() {
+	smallest_money(21);
+}
 
+void smallest_money(int pounds) {
+	int x, remainder = pounds, bin_int = 0;
+	reset_splits();
+	for(int i = 0; i<4; i++) {
+		x = remainder / denominations[i];
+		remainder = remainder - (x * denominations[i]);
+		splits[i] = x;
+		if(x > 0) { bin_int += pow(2,3-i); }
+	}
+	int count = 0;
+	int total_count = number_of_positives(bin_int);
 
-char plural(double x) { 
+	for(int i = 0; i< 4; i++) {
+		if (splits[i] != 0){
+			printf("%d £%d ", splits[i], denominations[i]);
+			if(i == 3) {
+				printf("pound coin%c", plural(splits[i]) );
+			} else { printf("note%c", plural(splits[i]) ); }
+
+			if (count != total_count) {
+				if (count == total_count - 1) { printf(" and "); } else { printf(", "); }
+				count++;
+			}
+		}
+		
+	}
+	printf("\n");
+}
+
+int number_of_positives(int binary_integer) {
+	int div = binary_integer, rem, count = 0;
+	while(div != 0) {
+		div = div/2;
+		rem = div % 2;
+		if(rem == 1) { count++; }
+	}
+	return count;
+}
+
+void reset_splits() {
+	splits[0] = 0; splits[1] = 0; splits[2] = 0; splits[3] = 0;
+}
+
+char plural(int x) { 
 	char plural_letter;
 	if (x == 1) { plural_letter=' '; } else { plural_letter='s'; }
 	return plural_letter;
 }
-
-int is_penultimate(int pounds, int y, int i) {
-	int denominations[4] = {20, 10, 5, 1};
-	int left_over = pounds;
-	int x, last_null;
-	for(int j = 0; j < 3; j++) {
-		x = left_over / denominations[j];
-		left_over = left_over - (x * denominations[j]);
-		if (last_null != -1) {
-			last_null = j;
-			printf("|%d|", j);
-		}
-	}
-
-	if (i == last_null-1) { return 1; }
-	
-	return -1;
-}
-
-void print_smallest_money(int pounds) {
-	int denominations[4] = {20, 10, 5, 1};
-	int left_over = pounds;
-	int next_x_left_over;
-	printf("£%d consits of", pounds);
-	int next_x,next_next_x, x;
-	for(int i = 0;i < 4; i++) {
-		
-		x = left_over / denominations[i];
-		left_over = left_over - (x * denominations[i]);
-		
-		if(x != 0) { // ends output when smallest denomination reached
-			if (i < 3) { next_x = left_over / denominations[i+1]; next_x_left_over = left_over - (x * denominations[i+1]);}
-			if (i < 2) { next_next_x = next_x_left_over / denominations[i+2]; }
-			printf(" %d £%d", x, denominations[i]); // prints maths logic
-			if(i != 3) { // only 1 pound coins are not notes
-				printf(" note%c", plural(x));
-
-				//dealing with "," and "and" grammar
-				// if (next_x != 0) {
-					if (is_penultimate(pounds, x, i) == 1) { printf("and"); } else { printf(",");}
-				// }
-				
-			}else {
-				printf(" pound coin%c", plural(x));
-			}
-			// if(i == 2) {
-			// 	if (next_x != 0) { printf("\band"); }
-			// }
-			
-		}
-		
-		
-	}
-	printf("\n");
-
-
-}
-
-int main() {
-	print_smallest_money(15);
-	print_smallest_money(79);
-	print_smallest_money(10);
-	print_smallest_money(26);
-}
-
